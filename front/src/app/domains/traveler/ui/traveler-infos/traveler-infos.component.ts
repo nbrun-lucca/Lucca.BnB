cf4ce91b5f54feee2c53ee7feb3f9dfcb045d270
Nicholas BRUN <nbrun@luccasoftware.com>
2025-12-11 16:21:50 +0100

    feat(traveler): add design for infos

--- front/src/app/domains/traveler/ui/traveler-infos/traveler-infos.component.ts
+++ front/src/app/domains/traveler/ui/traveler-infos/traveler-infos.component.ts
@@ -1,13 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { AppStore } from '@app/shared/data/stores/app.store';
-import { ButtonComponent } from '@lucca-front/ng/button';
-import { EmptyStateSectionComponent } from '@lucca-front/ng/empty-state';
+import { DividerComponent } from '@lucca-front/ng/divider';
+import { GridColumnComponent, GridComponent } from '@lucca-front/ng/grid';
 import { LinkComponent } from '@lucca-front/ng/link';
-import { LuUserDisplayModule, LuUserTileComponent } from '@lucca-front/ng/user';
 
 @Component({
   selector: 'lbnb-traveler-infos',
-  imports: [EmptyStateSectionComponent, LuUserDisplayModule, LuUserTileComponent, LinkComponent, ButtonComponent],
+  imports: [DividerComponent, GridColumnComponent, GridComponent, LinkComponent],
   templateUrl: './traveler-infos.component.html',
   styleUrl: './traveler-infos.component.scss',
 })
